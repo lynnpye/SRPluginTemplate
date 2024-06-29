44f7ebc5ded3bb11d812c076a05d18089bc90498
Lynn Pye <github@pyehouse.com>
2024-06-29 12:23:47 -0500

    not empty

---
+++ wrapping/wrap.cmd
@@ -0,0 +1,24 @@
+
+set SRPLUGINDIR=..
+REM location of 7zip utility
+set CMD7Z=7z
+
+rd /q /s plugins
+rd /q /s working
+
+mkdir plugins
+mkdir working
+
+for %%p in (SRRPlugin
+DFDCPlugin
+SRHKPlugin) do (
+rmdir /q /s %%p
+del %%p.zip
+xcopy /e /i ModBepInExCfg working\%%p
+xcopy configs\%%p.cfg working\%%p\BepInEx\config
+xcopy %SRPLUGINDIR%\%%p\bin\Debug\net35\%%p.dll working\%%p\BepInEx\plugins
+cd working\%%p
+%CMD7Z% a ..\..\plugins\%%p.zip BepInEx
+cd ..\..
+)
+
