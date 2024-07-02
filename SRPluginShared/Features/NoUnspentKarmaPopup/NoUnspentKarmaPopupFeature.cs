76d2e66bb90df6874cb8e50354f44d8a18db6093
Lynn Pye <github@pyehouse.com>
2024-07-02 14:39:53 -0500

    added new feature: no unspent karma popup

---
+++ SRPluginShared/Features/NoUnspentKarmaPopup/NoUnspentKarmaPopupFeature.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace SRPlugin.Features.NoUnspentKarmaPopup
+{
+    public class NoUnspentKarmaPopupFeature : FeatureImpl
+    {
+        private static ConfigItem<bool> CINoUnspentKarmaPopup;
+
+        public NoUnspentKarmaPopupFeature()
+            : base(new List<ConfigItemBase>()
+            {
+                (CINoUnspentKarmaPopup = new ConfigItem<bool>(FEATURES_SECTION, nameof(NoUnspentKarmaPopup), true, "block the popup asking you are you sure you wish to start this scene without spending unspent karma"))
+            })
+        {
+
+        }
+
+        public override void HandleEnabled()
+        {
+            SRPlugin.Harmony.PatchAll(typeof(UnspentKarmaListenerPatch));
+        }
+
+        public override void HandleDisabled()
+        {
+            SRPlugin.Harmony.Unpatch(
+                typeof(UnspentKarmaListener).GetMethod(nameof(UnspentKarmaListener.CreateUnspentKarmaPopup)),
+                typeof(UnspentKarmaListenerPatch).GetMethod(nameof(UnspentKarmaListenerPatch.CreateUnspentKarmaPopupPatch))
+                );
+        }
+
+        public static bool NoUnspentKarmaPopup
+        {
+            get
+            {
+                return CINoUnspentKarmaPopup.GetValue();
+            }
+
+            set
+            {
+                CINoUnspentKarmaPopup.SetValue(value);
+            }
+        }
+
+        [HarmonyPatch(typeof(UnspentKarmaListener))]
+        internal class UnspentKarmaListenerPatch
+        {
+            // Setting priority to VeryLow under the assumption that it means other plugins that
+            // patch this method will run before me and possibly block me. That's fine.
+            // If you were going to do something cool with this screen, more power to you.
+            // I just want to block the default behavior.
+            [HarmonyPrefix]
+            [HarmonyPriority(Priority.VeryLow)]
+            [HarmonyPatch(nameof(UnspentKarmaListener.CreateUnspentKarmaPopup))]
+            public static bool CreateUnspentKarmaPopupPatch(Player p)
+            {
+                // this is about as blunt as it gets
+                return !NoUnspentKarmaPopupFeature.NoUnspentKarmaPopup;
+            }
+        }
+    }
+}
--- SRPluginShared/Features/Readme.md
+++ SRPluginShared/Features/Readme.md
@@ -19,3 +19,26 @@ So having cheat buttons that grant the current player some bigger points is no b
 
 ##
 I keep playing with the "Features" thing. I'm goofing around with refactoring as I want to get to the point where you just create a new Feature implementation and you're good to go.
+
+##
+Oh yeah, need to get rid of those pesky "Unspent Karma" popups at the beginning of new levels. I take full responsibility for how my karma is spent at this point.
+
+And what about a way to dynamically add assets? Without having to rebake them into a new campaign.
+
+But what if it was in an optional plugin that didn't alter the original DLL, thus allowing other possibly otherwise conflicting additional mods to be installed and played with too, and thus enhancing my overall gameplay experience.
+*cough*
+You could just.... create a new score and release it, for someone to use on a play through the Dragonfall campaign, knowing they could do so without it otherwise affecting anything else they're doing. Or not, but that's on them. :)
+
+Eh... it's probably already done. Seriously, I guess I'm just a little surprised that the BepInEx thing never took root so far as I can tell. I see inquiries in a handful of places but I've not seen any indication it was routinely used.
+
+In the community for a game where hacking is so prevalent.
+
+I'm also a developer. I dunno, I guess I thought it was funny.
+
+Oh, and yeah, so apparently in SRHK in the cyberware screen, on subsequent passes through, you will see yourself capped at 6 Essence instead of 7 or more with things like Cyber Affinity. I mean... as far as I can tell, the appropriate block of code is returning the appropriate value.
+So I'm still working out what's going on in SRHK.
+
+Formatting a new external drive. Did not choose Quick since it's the first time. Hang onto your hats ladies and gentlemen, we are at 4%.
+
+And holding.
+
