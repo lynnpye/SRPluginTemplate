9b844b351e616549a8b7756cb4ff0ecf1343235a
Lynn Pye <github@pyehouse.com>
2024-07-08 07:30:59 -0500

    no longer shipping default .cfg files; let them be regenerated on a first run like normal

--- wrapping/wrap.cmd
+++ wrapping/wrap.cmd
@@ -15,7 +15,8 @@ SRHKPlugin) do (
 rmdir /q /s %%p
 del %%p.zip
 xcopy /e /i ModBepInExCfg working\%%p
-xcopy configs\%%p.cfg working\%%p\BepInEx\config
+REM no longer going to ship a starter .cfg
+REM xcopy configs\%%p.cfg working\%%p\BepInEx\config
 xcopy %SRPLUGINDIR%\%%p\bin\Debug\net35\%%p.dll working\%%p\BepInEx\plugins
 cd working\%%p
 %CMD7Z% a ..\..\plugins\%%p.zip BepInEx
