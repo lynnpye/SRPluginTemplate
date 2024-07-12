05a77f6bb1e8dce0ae9f3b03a97efc9b788ca48a
Lynn Pye <github@pyehouse.com>
2024-07-12 11:51:30 -0500

    fix: alwaysgofirst was breaking turn order

--- SRPluginShared/Features/AlwaysGoFirst/AlwaysGoFirstFeature.cs
+++ SRPluginShared/Features/AlwaysGoFirst/AlwaysGoFirstFeature.cs
@@ -29,6 +29,56 @@ namespace SRPlugin.Features.AlwaysGoFirst
 
         public static bool AlwaysGoFirst { get => CIAlwaysGoFirst.GetValue(); set => CIAlwaysGoFirst.SetValue( value ); }
 
+        public override void PostApplyPatches()
+        {
+            combatEventListener = new CombatEventListener();
+            KeyMessenger<string, global::GenericArgs>.AddKeyListener(GenericEvents.EVENT_ONCOMBATENTERED, new KeyMessenger<string, global::GenericArgs>.KeyDelegate(combatEventListener.OnCombatEntered));
+            KeyMessenger<string, global::GenericArgs>.AddKeyListener(GenericEvents.EVENT_ONCOMBATEXITED, new KeyMessenger<string, global::GenericArgs>.KeyDelegate(combatEventListener.OnCombatExited));
+#if SRHK
+            KeyMessenger<string, global::GenericArgs>.AddKeyListener(GenericEvents.EVENT_ONCOMBATINTENSE, new KeyMessenger<string, global::GenericArgs>.KeyDelegate(combatEventListener.OnCombatIntense));
+            KeyMessenger<string, global::GenericArgs>.AddKeyListener(GenericEvents.EVENT_ONCOMBATWRAPUP, new KeyMessenger<string, global::GenericArgs>.KeyDelegate(combatEventListener.OnCombatWrapup));
+#endif
+        }
+
+        public override void HandleDisabled()
+        {
+#if SRHK
+            KeyMessenger<string, global::GenericArgs>.RemoveKeyListener(GenericEvents.EVENT_ONCOMBATWRAPUP, new KeyMessenger<string, global::GenericArgs>.KeyDelegate(combatEventListener.OnCombatWrapup));
+            KeyMessenger<string, global::GenericArgs>.RemoveKeyListener(GenericEvents.EVENT_ONCOMBATINTENSE, new KeyMessenger<string, global::GenericArgs>.KeyDelegate(combatEventListener.OnCombatIntense));
+#endif
+            KeyMessenger<string, global::GenericArgs>.RemoveKeyListener(GenericEvents.EVENT_ONCOMBATEXITED, new KeyMessenger<string, global::GenericArgs>.KeyDelegate(combatEventListener.OnCombatExited));
+            KeyMessenger<string, global::GenericArgs>.RemoveKeyListener(GenericEvents.EVENT_ONCOMBATENTERED, new KeyMessenger<string, global::GenericArgs>.KeyDelegate(combatEventListener.OnCombatEntered));
+            combatEventListener = null;
+        }
+
+        private static bool RigRating { get; set; }
+        private static CombatEventListener combatEventListener = null;
+
+        private class CombatEventListener
+        {
+            public void OnCombatEntered(string key, global::GenericArgs inputs)
+            {
+                RigRating = true;
+            }
+
+            public void OnCombatExited(string key, global::GenericArgs inputs)
+            {
+                RigRating = false;
+            }
+
+#if SRHK
+            public void OnCombatIntense(string key, global::GenericArgs inputs)
+            {
+                RigRating = true;
+            }
+
+            public void OnCombatWrapup(string key, global::GenericArgs inputs)
+            {
+                RigRating = false;
+            }
+#endif
+        }
+
         [HarmonyPatch(typeof(TurnDirector))]
         internal class TurnDirectorPatch
         {
@@ -36,7 +86,7 @@ namespace SRPlugin.Features.AlwaysGoFirst
             [HarmonyPatch(nameof(TurnDirector.TeamRating))]
             public static void TeamRatingPostfix(ref int __result, Team t)
             {
-                if (!AlwaysGoFirst) return;
+                if (!AlwaysGoFirst || !RigRating) return;
 
                 List<Player> units = AccessTools.Field(typeof(Team), "units").GetValue(t) as List<Player>;
 
@@ -46,6 +96,8 @@ namespace SRPlugin.Features.AlwaysGoFirst
                 {
                     if (u.actorUID == SceneSingletonBehavior<TurnDirector>.Instance.PlayerZero.actorUID)
                     {
+                        SRPlugin.Squawk($"rigging team rating");
+                        RigRating = false;
                         __result = int.MaxValue;
                         return;
                     }
